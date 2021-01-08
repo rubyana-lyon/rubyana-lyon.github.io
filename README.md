7b89418c84aad1b9c93a533657b563273469d412
ruby-santin <rubyana.lyon@gmail.com>
2021-01-08 12:59:42 -0800

    added portfolio content

---
+++ README.md
@@ -0,0 +1,12 @@
+# rubyana88.github.io
+
+**Ruby Lyon Portfolio**
+
+Hello,
+I’m Ruby, an NYC-based product designer specializing in UI and UX design. I love all aspects of human-centered design and creating products from conception to completion. My extra special sauce is my ability to tie together design with my branding and marketing background to create joyful, accessible, and viable products.
+
+Welcome to my portfolio––please explore around and learn more about my projects and interests! 
+
+![Screenshot](portfolio.png)
+
+![Screenshot](portfolio2.png)
---
+++ js/wine.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //change navbar color as the user scrolls
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      console.log(scroll);
+      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        if (scroll > 200) {
+          $("nav").addClass("nav-scrolled");
+        }
+        else{
+          $("nav").removeClass("nav-scrolled");  	
+        }
+      } else {
+        if (scroll > 600) {
+          $("nav").addClass("nav-scrolled");
+        }
+        else{
+            $("nav").removeClass("nav-scrolled");  	
+        }
+    }
+  });
+  });
+
+  $('.custom-btn').on('click', function() {
+    window.open(
+      'https://www.figma.com/proto/KuVtHPH9102GPz051L0Qxq/Wine-Notes?node-id=436%3A5334&scaling=min-zoom',
+      '_blank'
+    );
+  })
